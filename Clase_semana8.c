f3390855b579bd45f1ec3ed832770857c4521983
Gabriela Montero <gabriela.montero@udla.edu.ec>
2026-05-14 15:02:40 -0500

    segunda version de codigo para imprimir un puntero de un vector

--- Clase_semana8.c
+++ Clase_semana8.c
@@ -6,16 +6,19 @@ int main()
 {
     
     
-  int vector[]= {3,6,9,12,15,18,21};
-  int i;
- 
+  int vector[7]= {3,6,9,12,15,18,21};
+
+ int *ptr;
+  ptr= &vector[0];
    
    for(int i=0; i< 7; i++)
    {
        printf("%d \n", vector[i]);
+       printf("%p \n", ptr);
    
    }
   
  
     return 0;
 }
+
